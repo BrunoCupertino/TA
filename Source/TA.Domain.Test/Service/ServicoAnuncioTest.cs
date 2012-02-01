9913b44074c9b71bc7afb94fd70594a8ee011516
bscupertino <bruno@transfereauto.com.br>
2012-01-31 22:53:10 -0200

    alteracoes de servico e inclusao de testes

---
+++ Source/TA.Domain.Test/Service/ServicoAnuncioTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TA.Domain.Service;
+using TA.Domain.Entity;
+using Moq;
+using TA.Domain.Repository;
+
+namespace TA.Domain.Test.Service
+{
+    [TestClass]
+    public class ServicoAnuncioTest
+    {
+        private Mock<IRepositorioAnuncio> repositorioMock;
+        private IServicoAnuncio target;
+
+        public ServicoAnuncioTest()
+        {
+            repositorioMock = new Mock<IRepositorioAnuncio>();
+            target = new ServicoAnuncio(repositorioMock.Object);
+        }
+
+        #region IServicoAnuncio Members
+
+        [TestMethod]
+        public void Anuciar_Deve_Chamar_Anunciar_Do_Repositorio()
+        {
+            var anuncio = new Anuncio();
+            
+
+            target.Anuciar(anuncio);
+            repositorioMock.Verify(r => r.Anuciar(anuncio), Times.Once());
+        }
+
+        [TestMethod]
+        public void AtualizarAnuncio(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ExcluirAnuncio(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public Anuncio ObterAnuncioPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Detalhar_Anuncio_Por_Id_Valido_Deve_Incrementar_Visitas_E_Atualizar()
+        {
+            var visitas = 50;
+            var anuncio = new Anuncio() { Id = 1, Status = StatusAnuncio.Aprovado, Visitas = visitas };
+            repositorioMock.Setup(r => r.ObterAnuncioPorId(anuncio.Id)).Returns(anuncio);
+
+            var anuncioSalvo = target.DetalharAnuncioPorId(anuncio.Id);
+
+            Assert.IsTrue(visitas + 1 == anuncioSalvo.Visitas);
+            repositorioMock.Verify(r => r.AtualizarAnuncio(anuncio));
+        }
+
+        [TestMethod]
+        public void ObterAnunciosDoAnunciante(Anunciante anunciante)
+        {
+        }
+
+        [TestMethod]
+        public List<Anuncio> ObterAnunciosEmDestaque()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public List<Anuncio> ObterAnunciosMaisVisitados()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public List<Anuncio> ObterAnunciosRecentes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void AprovarAnuncio(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void AprovarAnunciosPorId(List<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DesaprovarAnuncio(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DesaprovarAnunciosPorId(List<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public List<Anuncio> ObterAnunciosAguardandoAprovacao()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/TA.Domain/Service/ServicoAnuncio.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TA.Domain.Entity;
+using TA.Domain.Repository;
+
+namespace TA.Domain.Service
+{
+    public class ServicoAnuncio : IServicoAnuncio
+    {
+        private IRepositorioAnuncio repositorioDeAnuncios;
+
+        public ServicoAnuncio(IRepositorioAnuncio repositorioDeAnuncios)
+        {
+            this.repositorioDeAnuncios = repositorioDeAnuncios;
+        }
+
+        #region IServicoAnuncio Members
+
+        public void Anuciar(Anuncio anuncio)
+        {
+            //validar
+            this.repositorioDeAnuncios.Anuciar(anuncio);
+        }
+
+        public void AtualizarAnuncio(Anuncio anuncio)
+        {
+            //validar
+            this.repositorioDeAnuncios.AtualizarAnuncio(anuncio);
+        }
+
+        public void ExcluirAnuncio(Anuncio anuncio)
+        {
+            //validar
+            this.repositorioDeAnuncios.ExcluirAnuncio(anuncio);
+        }
+
+        public Anuncio ObterAnuncioPorId(int id)
+        {
+            return this.repositorioDeAnuncios.ObterAnuncioPorId(id);
+        }
+
+        public Anuncio DetalharAnuncioPorId(int id)
+        {
+            Anuncio anuncio = this.ObterAnuncioPorId(id);
+            anuncio.Visitas++;
+            this.AtualizarAnuncio(anuncio);
+            return anuncio;
+        }
+
+        public List<Anuncio> ObterAnunciosDoAnunciante(Anunciante anunciante)
+        {
+            return this.repositorioDeAnuncios.ObterAnunciosDoAnunciante(anunciante);
+        }
+
+        public List<Anuncio> ObterAnunciosEmDestaque()
+        {
+            return this.repositorioDeAnuncios.ObterAnunciosEmDestaque();
+        }
+
+        public List<Anuncio> ObterAnunciosMaisVisitados()
+        {
+            return this.repositorioDeAnuncios.ObterAnunciosMaisVisitados();
+        }
+
+        public List<Anuncio> ObterAnunciosRecentes()
+        {
+            return this.ObterAnunciosRecentes();
+        }
+
+        public void AprovarAnuncio(Anuncio anuncio)
+        {
+            anuncio.Status = StatusAnuncio.Aprovado;
+            this.AtualizarAnuncio(anuncio);
+        }
+
+        public void AprovarAnunciosPorId(List<int> ids)
+        {
+            this.repositorioDeAnuncios.ObterAnunciosPorIds(ids).ForEach(e => this.AprovarAnuncio(e));
+        }
+
+        public void DesaprovarAnuncio(Anuncio anuncio)
+        {
+            anuncio.Status = StatusAnuncio.Desaprovado;
+            this.AtualizarAnuncio(anuncio);
+        }
+
+        public void DesaprovarAnunciosPorId(List<int> ids)
+        {
+            this.repositorioDeAnuncios.ObterAnunciosPorIds(ids).ForEach(e => this.DesaprovarAnuncio(e));
+        }
+
+        public List<Anuncio> ObterAnunciosAguardandoAprovacao()
+        {
+            return this.repositorioDeAnuncios.ObterAnunciosAguardandoAprovacao();
+        }
+
+        #endregion
+    }
+}
